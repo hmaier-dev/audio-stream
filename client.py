64a1f3a8a3ba4d90b539bce6048c27be9ef83826
Hendrik Maier <hmaier@ipb-halle.de>
2023-05-18 17:18:22 +0200

    changing the socket from using tcp to udp

--- client.py
+++ client.py
@@ -19,14 +19,14 @@ RATE = 44100
 # WAVE_OUTPUT_FILENAME = "client_temp.wav"
 
 
-
 def receive_audio():
     # server_name = socket.gethostname()
 
     server_name = "192.168.0.224"
     port = 61234  # port of the server
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4 and TCP
+    client_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_DGRAM)  # Using IPv4 and UDP
     print("Trying to connect to {}:{}".format(server_name, port))
     client_socket.connect((server_name, port))
     print("Success!")
@@ -43,30 +43,37 @@ def receive_audio():
     frames = []
     # client_socket.send(data.encode())# initial connection to the server
     data = b""
-    payload_size = struct.calcsize("Q") # 8 bytes
+    payload_size = struct.calcsize("Q")  # 8 bytes
 
     while True:
 
         try:
-            while len(data) < payload_size: #receiving 1016 bytes
+            while len(data) < payload_size:  # receiving 1016 bytes
                 # this while loop could be missed (because it iterates jsut once),
                 # but for checking if theres a packet, I leave it there
-                packet = client_socket.recv(CHUNK) #receive the first part of the data to unpack the data
-                if not packet: break
-                data+=packet
+                # receive the first part of the data to unpack the data
+                packet = client_socket.recv(CHUNK)
+                if not packet:
+                    break
+                data += packet
 
-            packed_msg_size = data[:payload_size] # var for calculation of the message size
+            # var for calculation of the message size
+            packed_msg_size = data[:payload_size]
             data = data[payload_size:]
-            msg_size = struct.unpack("Q", packed_msg_size)[0] # calc how big the message will be
-            #msg_size = 4106
-            while len(data) < msg_size: #receive 3090 bytes
+            # calc how big the message will be
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            # msg_size = 4106
+            while len(data) < msg_size:  # receive 3090 bytes
                 data += client_socket.recv(CHUNK)
 
-            frame_data = data[:msg_size] # deconstructing the packet, to get the actual audio data
-            data = data[msg_size:] # deconstructing the packet (maybe I don't need this?!)
-            frame = pickle.loads(frame_data) # convert byte stream into audio
-            frames.append(frame) # write the audio to a array which will create the client_temp.wav
-            stream.write(frame) #output the audio to the pc
+            # deconstructing the packet, to get the actual audio data
+            frame_data = data[:msg_size]
+            # deconstructing the packet (maybe I don't need this?!)
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)  # convert byte stream into audio
+            # write the audio to a array which will create the client_temp.wav
+            frames.append(frame)
+            stream.write(frame)  # output the audio to the pc
             stream.get_write_available()
         except KeyboardInterrupt:
             client_socket.close()
